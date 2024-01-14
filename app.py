1172c562dfd17cef7b7a066a6d50ad8c324285eb
Felix H <felixhuang101@gmail.com>
2024-01-14 16:05:02 -0500

    added api endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
-from flask import Flask, request,render_template
+import pandas as pd
+from flask import Flask, request,render_template, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict/',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -21,6 +22,18 @@ def predict():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='House price should be $ {}'.format(output))
+
+@app.route('/api/')
+def api():
+    bed_room = request.args.get('bed_room')
+    area = request.args.get('area')
+    house_age = request.args.get('house_age')
+
+    df = pd.DataFrame({'bed_room':[bed_room], 'area':[area], 'house_age':[house_age]})
+
+    pred_price = model.predict(df)
+
+    return jsonify({'House Price': str(pred_price)})
 
 if __name__ == "__main__":
     app.run(debug=True)
